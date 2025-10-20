1bae80a93ebbb75e34ee834249eaa9e2bb336de1
Cristian Alberto Lopez Lombana <calopez@stratio.com>
2025-10-20 14:37:11 -0500

    feat: 	              -Crear case clases UDFs

---
+++ src/main/scala/com/stratio/sparta/TransformationExampleUdfs.scala
@@ -0,0 +1,26 @@
+package com.stratio.sparta
+
+import com.stratio.sparta.sdk.lite.common.SpartaUDF
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
+
+case class ToUpperCaseUDF() extends SpartaUDF {
+
+  val name = "uppercaseSparta"
+
+  private val upper: String => String = _.toUpperCase
+
+  val userDefinedFunction: UserDefinedFunction = udf(upper)
+
+}
+
+case class ConcatUDF() extends SpartaUDF {
+
+  val name = "concatSparta"
+
+  private val concat: (String, String) => String =  { case (str1, str2) =>
+    s"$str1/$str2"
+  }
+
+  val userDefinedFunction: UserDefinedFunction = udf(concat)
+}
